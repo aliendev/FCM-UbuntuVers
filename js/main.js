366267e18c09075ab78771677ee59a841f8d782a
Michael Youngblood <yb@aliendev.com>
2012-12-08 22:04:27 -0800

    form writes to screen now

--- js/main.js
+++ js/main.js
@@ -16,6 +16,26 @@ function processForm(formElement) {
     if (formElement.preventDefault) { formElement.preventDefault() };
     console.log('form has been processed.');
 
+    // let's console log whatever you type into the form
+    // console logging the whole element will show you the object your working with, and what you have access too. This is a great hint on figuring out what you can do directly.
+    console.log(formElement);
+
+    // I like to give all my form elements their own id's to give myself access outside of the form as well as simple access inside of it
+    ubuVersNumValue = ge('ubuVersNum').value;
+    ubuVersNameValue = ge('ubuVersName').value;
+    console.log(ubuVersNumValue);
+    console.log(ubuVersNameValue);
+
+    if (ubuVersNumValue != '') {
+        var list = ge('ubuntuVersionsDisplayList'),
+            listItem = document.createElement('li'),
+            listText = ubuVersNumValue + ": " + ubuVersNameValue;
+
+        listItem.innerHTML = listText;
+        list.appendChild(listItem);
+    }
+
+
     // Return false to prevent the default form behavior
     return false;
 }
@@ -23,8 +43,8 @@ function processForm(formElement) {
 /*******************
 // Variables
 *******************/
-var form = ge('ubuVersForm'); // form contains ubuVersForm element
-
+var form = ge('ubuVersForm'), // form contains ubuVersForm element
+    ubuVersNumValue = '';
 
 
 
